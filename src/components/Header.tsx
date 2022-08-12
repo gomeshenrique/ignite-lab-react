e16975aa84c0bc3127ec45842b17a8e26b03a6f8
Your Name <you@example.com>
2022-08-12 10:05:30 -0300

    Ignite Lab - Rocketseat

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+      <h1 className="font-semibold">
+        ignite lab{" "}
+        <span className="font-extrabold text-green-500 text-2xl">|</span>{" "}
+        REACTJS
+      </h1>
+    </header>
+  );
+}
